b11953845e92309a92ee02fb8bf60d4ed945dc72
Minhajul Qowim <78860546+qowimdev@users.noreply.github.com>
2024-01-14 23:52:42 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,93 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  } else {
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// Carousel
+new Swiper("#swiper-2", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 4000,
+  },
+  pagination: {
+    el: "#swiper-2 .swiper-custom-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      // renderBullet: function (index, className) {
+      return `<div class="${className}">
+            <span class="number">${index + 1}</span>
+            <span class="line"></span>
+            </div>`;
+    },
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 1.5,
+    },
+    1400: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
+
